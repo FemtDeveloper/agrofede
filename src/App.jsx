e6e8ba5333b24b1de376c6f4e4931bece7441dbf
Felix Miranda <feedmite@hotmail.com>
2022-05-26 19:41:36 -0500

    cambio de backgroun a transparente y tamaño logo principal

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./components/Register/Register";
 import Home from "./components/Home/Home";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -4,20 +4,7 @@ import vegetales from "../../assets/imgs/vegetables.webp";
 import "./News.css";
 
 const News = () => {
-  const [agricultorsList, setAgricultorsList] = useState([]);
-
-  useEffect(() => {
-    const agricultors = axios
-      .get("http://localhost:1337/api/agricultors?populate=*")
-      .then((res) => {
-        setAgricultorsList(res.data.data);
-      });
-  }, []);
-
-  console.log(agricultorsList);
-  agricultorsList.map((a) => {
-    console.log(a.attributes.nombre);
-  });
+  //
 
   return (
     <div className="news-container">
