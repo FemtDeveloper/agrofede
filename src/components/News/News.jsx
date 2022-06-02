4db26442c4976a135064c2c56f76d5d0e6f7815e
Alconsc <consuegraalberto@hotmail.com>
2022-06-02 16:02:08 -0500

    backgron footer y mision

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,30 +1,29 @@
 import React from "react";
-import vegetales from "../../assets/imgs/vegetables.webp";
+import bancredito from "../../assets/imgs/bancredito.jpg";
+import trabajoRemunerado from "../../assets/imgs/trabajoRemunerado.webp";
+import alzaenal from "../../assets/imgs/alzaenal.jpg";
 import "./News.css";
 
 const News = () => {
   return (
     <div className="news-container">
-      <p className="text-muted h4 mr-auto">Ultimas Noticias</p>
+      <p className="text-muted h2 mr-auto">Ultimas Noticias</p>
       <article>
-        <a href="https://fonts.google.com/" target="_blank">
-          <img src={vegetales} alt="" />
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-          iste explicabo cupiditate?
+        <a href="https://www.larepublica.co/economia/31-2-de-las-mujeres-en-zonas-rurales-se-encuentran-en-situacion-de-pobreza-3370282" target="_blank">
+          <img src={trabajoRemunerado} alt="" />
+          En zonas rurales 31,2% corresponde a las mujeres que están en situación de pobreza
         </a>
       </article>
       <article>
-        <img src={vegetales} alt="" />
-        <a href="">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-          iste explicabo cupiditate?
+        <a href="https://www.minagricultura.gov.co/noticias/Paginas/M%C3%A1s-de-26-000-emprendedores-rurales-podr%C3%A1n-acceder-a-l%C3%ADnea-de-microcr%C3%A9dito-gracias-a-convenio-firmado-entre-El-Campo-Empren.aspx">
+          <img src={alzaenal} alt="" />
+          Más de 26.000 emprendedores rurales podrán acceder a línea de microcrédito gracias a convenio firmado entre El Campo Emprende y Bancamía
         </a>
       </article>
       <article>
-        <img src={vegetales} alt="" />
-        <a href="">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-          iste explicabo cupiditate?
+        <a href="https://digital.bancoagrario.gov.co/api/campaigns/referral/572b0cd9-ff2e-4159-8a27-82a94721308f">
+          <img src={bancredito} alt="" />
+          Solicita tu CREDITO al Banco Agrario en LINEA!!!!!
         </a>
       </article>
     </div>
